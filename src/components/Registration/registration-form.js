f7acb0e03bcc1e61a6a4a4fec139d988c8ad50c1
Dylan <nielsen.dylan@gmail.com>
2018-04-18 16:13:42 -0700

    Add react tabs

--- src/components/Registration/registration-form.js
+++ src/components/Registration/registration-form.js
@@ -4,6 +4,7 @@ import {registerUser} from '../../actions/users';
 import {login} from '../../actions/auth';
 import Input from '../Login/Input';
 import {required, nonEmpty, matches, length, isTrimmed} from '../../validators';
+import './registration_form.css';
 const passwordLength = length({min: 10, max: 72});
 const matchesPassword = matches('password');
 
--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -1,31 +1,47 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Link, Redirect} from 'react-router-dom';
-import './landing-page.css'
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import './landing-page.css';
 import LoginForm from './Login/Login-form';
-import skyrimLogo from './images/logo.png'
+import skyrimLogo from './images/logo.png';
+import RegistrationForm from './Registration/registration-form';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import './react-tabs.css';
 export function LandingPage(props) {
-    // If we are logged in redirect straight to the user's dashboard
-    if (props.loggedIn) {
-        return <Redirect to="/dashboard" />;
-    }
+  // If we are logged in redirect straight to the user's dashboard
+  if (props.loggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
 
-    return (
-        <div className="landing-page">
-            <h2>Spaced Repition App</h2>
-            <LoginForm />
-            <Link to="/register">Register</Link>
-            <div className="app-info"> <h5></h5>
-            <img className="app-img" src={skyrimLogo} alt="logo"/> 
-            <p className="app-overview"></p>
-            </div>
-           
-            </div>
-    );
+  return <div className="landing-page">
+      <h2>Spaced Repition: Learn Dragon Language</h2>
+
+      <Tabs>
+        <TabList>
+          <Tab>Register</Tab>
+          <Tab>Log In</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Register</h2>
+          <RegistrationForm />
+        </TabPanel>
+        <TabPanel>
+          <h2>Log In</h2>
+          <LoginForm />
+        </TabPanel>
+      </Tabs>
+      <p className="app-overview">
+        Now is your chance to learn dragon language
+      </p>
+      <div className="app-info">
+        <img className="app-img" src={skyrimLogo} alt="logo" />
+      </div>
+    </div>;
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.auth.currentUser !== null
+  loggedIn: state.auth.currentUser !== null
 });
 
 export default connect(mapStateToProps)(LandingPage);
